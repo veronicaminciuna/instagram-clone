2c384be9113de72f256eb7a0ecb057befac10f8e
Veronica Minciuna <veronicaminci@gmail.com>
2022-05-12 16:18:19 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='app_header'>
+        <img 
+          className='app_headerImage'
+          src='https://instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png'
+          alt=""
+        />
+      </div>
+      <h1>Hi There</h1>
+
+    </div>
+  );
+}
+
+export default App;
